a0e7f24bf1d3f1a707d9af6e3f69a5fc7d557996
Swati Navalkar <SwatiSNavalkar@gmail.com>
2018-08-14 17:18:32 +0530

    better logic
    changed the logic for calculating steps which eliminated complicated loops

--- CoastGuard.java
+++ CoastGuard.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class CoastGuard {
+public class CoastGuard2 {
 	int M;	//no. of rows
 	int N;	//no. of columns
 	int k;	//no. of boats
@@ -8,13 +8,33 @@ public class CoastGuard {
 	int[][] boats;	//array of all boat step information
 	int grids;	//no. of grids (M*N)
 	
+	public void calUncontSq() {
+		boats = new int[k][grids];
+		for(int i = 0, j = 0; i < k; i++){	//make k arrays of boat individual boat steps in boats' array
+			for(int a = 0; a < N; a++) {
+				for(int b = 0; b < M; b++, j++) {
+					boats[i][j] = Math.abs(positions[i][0] - a) + Math.abs(positions[i][1] - b);	//steps required by the boat to reach every grid
+				}
+			}
+			j = 0;
+		}
+		uncontSq = 0;
+		for(int i = 0; i < (grids); i++){	//compare step values of all boats in same grids
+			for(int b = 0; b < k-1; b++){
+				if(boats[b][i] == boats[b+1][i]){
+					uncontSq++;
+					break;
+				}
+			}
+		}
+		System.out.println("Number of uncontrolled grids are " + uncontSq);
+	}
+	
 	public void getInput() {
-		String inputLine = null;
 		String[] lineVector = null;
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter comma separated values of rows, columns, no. of boats: ");
-		inputLine = in.nextLine();
-		lineVector = inputLine.split(",");
+		lineVector = in.nextLine().split(",");
 		try {
 			M = Integer.parseInt(lineVector[0]);
 			N = Integer.parseInt(lineVector[1]);
@@ -24,8 +44,7 @@ public class CoastGuard {
 			positions = new int[k][2];
 			System.out.println("Enter comma separated values of position of boats line by line: ");
 			for(int i = 0; i < k; i++) {
-				inputLine = in.nextLine();
-				lineVector = inputLine.split(",");
+				lineVector = in.nextLine().split(",");
 				int left = Integer.parseInt(lineVector[0]);
 				int right = Integer.parseInt(lineVector[1]);
 				int[] num = {left, right};
@@ -60,54 +79,8 @@ public class CoastGuard {
 		System.exit(1);
 	}
 	
-	public void calUncontSq() {
-		boats = new int[k][grids];
-		
-		for(int i = 0; i < k; i++){	//make k arrays of boat individual boat steps in boats' array
-			int j = 0;	//variable to denote grids
-			boats[i][j] = positions[i][0] + positions[i][1];	//no. of steps in first grid
-			int starting = boats[i][0];
-			int left = positions[i][0];
-			for(int a = 0; a < N; a++) {
-				starting = boats[i][j];
-				for(int n = 0; n < (M - 1); n++) {
-					int right = positions[i][1];
-					if(right > 0) {
-						j++;
-						boats[i][j] = boats[i][j-1] - 1;
-						right--;
-					}
-					else {
-						j++;
-						boats[i][j] = boats[i][j-1] + 1;
-					}
-				}
-				j++;
-				if(j < grids){
-					if(left > 0){
-						boats[i][j] = starting - 1;
-						left--;
-					}
-					else {
-						boats[i][j] = starting + 1;
-					}
-				}
-			}
-		}
-		uncontSq = 0;
-		for(int i = 0; i < (grids); i++){	//compare step values of all boats in same grids
-			for(int b = 0; b < k-1; b++){
-				if(boats[b][i] == boats[b+1][i]){
-					uncontSq++;
-					break;
-				}
-			}
-		}
-		System.out.println("Number of uncontrolled grids are " + uncontSq);
-	}
-	
 	public static void main(String[] args) {
-		CoastGuard c = new CoastGuard();
+		CoastGuard2 c = new CoastGuard2();
 		c.getInput();
 		c.calUncontSq();
 	}
